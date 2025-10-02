013ac6e5dd74ea7cbc927466d92da2c7e2b9a81c
taciano-pacchialat <taciano3003@gmail.com>
2025-10-02 12:45:54 -0300

    preprocesamiento de imagen basico

--- pruebas/open-cv/src/main.py
+++ pruebas/open-cv/src/main.py
@@ -14,7 +14,15 @@ def main():
             print("Failed to capture frame")
             break
 
-        cv2.imshow("Webcam", frame)
+        # Filtra la imagen para reducir ruido y detalles innecesarios.
+        blurred_frame = cv2.GaussianBlur(frame, (11, 11), 0)
+
+        # HSV = Hue, Saturation, Value (brightness).
+        # Convierte la imagen desde formato BGR a HSV (son formatos distintos de imagen)
+        hsv_frame = cv2.cvtColor(blurred_frame, cv2.COLOR_BGR2HSV)
+        cv2.imshow(
+            "Webcam", hsv_frame
+        )  # Se ve medio cursed, pero sirve para procesar imagenes
 
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
