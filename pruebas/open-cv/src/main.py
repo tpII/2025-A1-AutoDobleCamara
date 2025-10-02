c93290949fdd6d0c332441b0b0a9a0e8c8ed905a
taciano-pacchialat <taciano3003@gmail.com>
2025-10-02 12:31:37 -0300

    captura de video por webcam

---
+++ pruebas/open-cv/src/main.py
@@ -0,0 +1,27 @@
+import cv2
+
+
+def main():
+    # Desde la webcam
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print("Error: Could not open camera.")
+        return
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Failed to capture frame")
+            break
+
+        cv2.imshow("Webcam", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
